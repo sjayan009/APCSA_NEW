3c1985e9033ed62a403fca411cef719da5f01146
sjayan009 <sjayan009@gmail.com>
2023-05-20 15:17:32 -0400

    init

--- Main.java
+++ Main.java
@@ -116,8 +116,6 @@ public class Main extends JFrame
     }
     public static void main(String[] args) 
     {
-        System.out.println("Hello!");
-        System.out.println("Bye");
         new Main();
     }    
 }
